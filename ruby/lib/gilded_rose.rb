d407c2d97958543b8914cbcd803e881d7d16249b
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-30 14:00:07 +0000

    Passing the first test: normal_update_quality

---
+++ ruby/lib/gilded_rose.rb
@@ -0,0 +1,74 @@
+class GildedRose
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+
+  def normal_update_quality
+    @sell_in -= 1
+    # return if @item.quality == 0
+
+    @quality -= 1
+    # @quality -= 1 if @sell_in <= 0
+  end
+
+
+  def update_quality
+    case name
+      when 'normal'
+        return normal_update_quality
+
+
+      # # Normal
+      #   if item.quality < 50
+      #     item.quality = item.quality + 1
+      # # Backstage
+      #     if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      #       #increase by 2 when there are 10 days or less
+      #       if item.sell_in < 11
+      #         if item.quality < 50
+      #           item.quality = item.quality + 1
+      #         end
+      #       end
+      #       #increase by 3 when there are 5 days or less
+      #       if item.sell_in < 6
+      #         if item.quality < 50
+      #           item.quality = item.quality + 1
+      #         end
+      #       end
+      #     end
+      #   end
+      # end
+      # #Sulfuras
+      # if item.name != "Sulfuras, Hand of Ragnaros"
+      #   item.sell_in = item.sell_in - 1
+      # end
+      # #sell by date has passed
+      # if item.sell_in < 0
+      #   if item.name != "Aged Brie"
+      #     if item.name != "Backstage passes to a TAFKAL80ETC concert"
+      #       if item.quality > 0
+      #         if item.name != "Sulfuras, Hand of Ragnaros"
+      #           item.quality = item.quality - 1
+      #         end
+      #       end
+      #     else
+      #       item.quality = item.quality - item.quality
+      #     end
+      #   else
+      #     if item.quality < 50
+      #       item.quality = item.quality + 1
+      #     end
+      #   end
+    end
+    end
+
+    def to_s()
+      "#{@name}, #{@sell_in}, #{@quality}"
+    end
+
+    end
---
+++ ruby/spec/gilded_rose_spec.rb
@@ -0,0 +1,14 @@
+require File.join(File.dirname(__FILE__), '../lib/gilded_rose')
+
+describe GildedRose do
+
+  describe "#normal_update_quality" do
+    it "normal rose's sell in date decrease by 1" do
+      item = GildedRose.new("normal", 10, 10)
+      item.normal_update_quality()
+      expect(item.quality).to eq(9)
+      expect(item.sell_in).to eq(9)
+    end
+  end
+
+end
